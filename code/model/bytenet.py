187a4eb6cfc26069150c8d0bb33ff8db83ed276d
Andreas Madsen <amwebdk@gmail.com>
2017-01-13 10:56:28 +0100

    [cleanup] add name_scopes and variable_scopes

--- code/model/bytenet.py
+++ code/model/bytenet.py
@@ -13,24 +13,27 @@ from dataset.abstract.text_dataset import TextDataset
 def sg_res_block(tensor, opt):
     # default rate
     opt += stf.sg_opt(size=3, rate=1, causal=False)
+    default_name = f"lyr-res-block-{opt.size}-{opt.rate}-{opt.causal}"
 
-    # input dimension
-    in_dim = tensor.get_shape().as_list()[-1]
+    with stf.variable_scope(opt.name, default_name, [tensor]):
+        # input dimension
+        in_dim = tensor.get_shape().as_list()[-1]
 
-    # reduce dimension
-    input_ = (tensor
-              .sg_bypass(act='relu', bn=(not opt.causal), ln=opt.causal)
-              .sg_conv1d(size=1, dim=in_dim // 2,
-                         act='relu', bn=(not opt.causal), ln=opt.causal))
+        # reduce dimension
+        input_ = (tensor
+                  .sg_bypass(act='relu', bn=(not opt.causal), ln=opt.causal)
+                  .sg_conv1d(size=1, dim=in_dim // 2,
+                             act='relu', bn=(not opt.causal), ln=opt.causal))
 
-    # 1xk conv dilated
-    out = input_.sg_aconv1d(size=opt.size, rate=opt.rate, causal=opt.causal,
-                            act='relu', bn=(not opt.causal), ln=opt.causal)
+        # 1xk conv dilated
+        out = input_.sg_aconv1d(size=opt.size, rate=opt.rate,
+                                causal=opt.causal,
+                                act='relu', bn=(not opt.causal), ln=opt.causal)
 
-    # dimension recover and residual connection
-    out = out.sg_conv1d(size=1, dim=in_dim) + tensor
+        # dimension recover and residual connection
+        out = out.sg_conv1d(size=1, dim=in_dim) + tensor
 
-    return out
+        return out
 
 
 # TODO: kill this
@@ -52,67 +55,74 @@ class ByteNet(Model):
     def _build_model(self, x: stf.Tensor, y_src: stf.Tensor) -> stf.Tensor:
         # make embedding matrix for source and target
         emb_x = stf.sg_emb(
-            name='embedding_source',
+            name='embedding-source',
             voca_size=self.dataset.vocabulary_size,
             dim=self.latent_dim
         )
         emb_y = stf.sg_emb(
-            name='embedding_target',
+            name='embedding-target',
             voca_size=self.dataset.vocabulary_size,
             dim=self.latent_dim
         )
 
-        # embed table lookup
-        enc = x.sg_lookup(emb=emb_x)
-
         #
         # encode graph ( atrous convolution )
         #
+        with stf.name_scope(None, "encoder", values=[x, emb_x]):
 
-        # loop dilated conv block
-        for i in range(self.num_blocks):
-            enc = (enc
-                   .sg_res_block(size=5, rate=1)
-                   .sg_res_block(size=5, rate=2)
-                   .sg_res_block(size=5, rate=4)
-                   .sg_res_block(size=5, rate=8)
-                   .sg_res_block(size=5, rate=16))
+            # embed table lookup
+            enc = x.sg_lookup(emb=emb_x)
 
-        # concat merge target source
-        enc = enc.sg_concat(target=y_src.sg_lookup(emb=emb_y))
+            # loop dilated conv block
+            for i in range(self.num_blocks):
+                with stf.name_scope(None, "lyr-encoder", values=[enc]):
+                    enc = (enc
+                           .sg_res_block(size=5, rate=1)
+                           .sg_res_block(size=5, rate=2)
+                           .sg_res_block(size=5, rate=4)
+                           .sg_res_block(size=5, rate=8)
+                           .sg_res_block(size=5, rate=16))
 
         #
         # decode graph ( causal convolution )
         #
+        with stf.name_scope(None, "decoder", values=[enc, y_src, emb_y]):
+            dec = enc.sg_concat(target=y_src.sg_lookup(emb=emb_y))
 
-        # loop dilated causal conv block
-        dec = enc
-        for i in range(self.num_blocks):
-            dec = (dec
-                   .sg_res_block(size=3, rate=1, causal=True)
-                   .sg_res_block(size=3, rate=2, causal=True)
-                   .sg_res_block(size=3, rate=4, causal=True)
-                   .sg_res_block(size=3, rate=8, causal=True)
-                   .sg_res_block(size=3, rate=16, causal=True))
+            # loop dilated causal conv block
+            for i in range(self.num_blocks):
+                with stf.name_scope(None, "lyr-decoder", values=[dec]):
+                    dec = (dec
+                           .sg_res_block(size=3, rate=1, causal=True)
+                           .sg_res_block(size=3, rate=2, causal=True)
+                           .sg_res_block(size=3, rate=4, causal=True)
+                           .sg_res_block(size=3, rate=8, causal=True)
+                           .sg_res_block(size=3, rate=16, causal=True))
 
-        # final fully convolution layer for softmax
-        return dec.sg_conv1d(size=1, dim=self.dataset.vocabulary_size)
+            # final fully convolution layer for softmax
+            return dec.sg_conv1d(size=1, dim=self.dataset.vocabulary_size)
 
     def train(self, max_ep=20):
-        # get source and target tensors
-        x = stf.cast(self.dataset.source, stf.int32)
-        y = stf.cast(self.dataset.target, stf.int32)
+        with stf.name_scope(None, "preprocessing",
+                            values=[self.dataset.source, self.dataset.target]):
+            # get source and target tensors
+            x = stf.cast(self.dataset.source, stf.int32)
+            y = stf.cast(self.dataset.target, stf.int32)
 
-        # shift target for training source
-        y_src = stf.concat(1, [
-            stf.zeros((stf.shape(y)[0], 1), y.dtype),  # first value is zero
-            y[:, :-1]  # skip last value
-        ])
+            # shift target for training source
+            y_src = stf.concat(1, [
+                # first value is zero
+                stf.zeros((stf.shape(y)[0], 1), y.dtype),
+                # skip last value
+                y[:, :-1]
+            ])
 
-        dec = self._build_model(x, y_src)
+        with stf.name_scope(None, "model", values=[x, y_src]):
+            dec = self._build_model(x, y_src)
 
-        # cross entropy loss with logit and mask
-        loss = dec.sg_ce(target=y, mask=True)
+        with stf.name_scope(None, "optimization", values=[dec, y]):
+            # cross entropy loss with logit and mask
+            loss = dec.sg_ce(target=y, mask=True)
 
         # train
         stf.sg_train(log_interval=30,
--- code/script/bytenet_synthetic_digits_train.py
+++ code/script/bytenet_synthetic_digits_train.py
@@ -7,6 +7,6 @@ from model import ByteNet
 # set log level to debug
 stf.sg_verbosity(10)
 
-dataset = SyntheticDigits(batch_size=16, examples=10000, seed=10)
+dataset = SyntheticDigits(batch_size=16, examples=100, seed=10)
 model = ByteNet(dataset, num_blocks=3, latent_dim=20)
 model.train(max_ep=100)
