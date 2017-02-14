dda5e08d8152316aa4d6cc9b7c58c41f8ff84160
Andreas Madsen <amwebdk@gmail.com>
2017-02-14 15:22:56 +0100

    [script] add WMT training script and setup qsub

---
+++ code/script/bytenet_wmt_bilingual_news_train.py
@@ -0,0 +1,14 @@
+
+from code.dataset import WMTBilingualNews
+from code.model import ByteNet
+from code.metric import BleuScore, ModelLoss
+
+dataset_train = WMTBilingualNews(batch_size=16,
+                                 year=2014, source_lang='de', target_lang='en')
+dataset_test = WMTBilingualNews(batch_size=16,
+                                year=2015, source_lang='de', target_lang='en')
+
+model = ByteNet(dataset_train, save_dir='asset/bytenet_wmt_2014')
+model.add_metric(BleuScore(dataset_test))
+model.add_metric(ModelLoss(dataset_test))
+model.train(max_ep=60, lr=0.0001)
---
+++ qsub/bytenet_wmt_bilingual_news.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#PBS -N bytenet-wmt-bilingual-news
+#PBS -l walltime=12:00:00
+#PBS -l nodes=1:ppn=4:gpus=1
+#PBS -m eba
+#PBS -M amwebdk@gmail.com
+#PBS -q k40_interactive
+
+cd $PBS_O_WORKDIR
+
+# Enable python3
+export PYTHONPATH=./
+source ~/stdpy3/bin/activate
+
+CUDA_VISIBLE_DEVICES=3 python3 code/script/bytenet_wmt_bilingual_news_train.py
