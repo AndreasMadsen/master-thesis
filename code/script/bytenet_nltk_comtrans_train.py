6698397ae8bf6185502117b891e836127d8a8af4
Andreas Madsen <amwebdk@gmail.com>
2017-02-17 14:46:43 +0100

    [feature] add metrics to nltk comtrans train

--- code/script/bytenet_nltk_comtrans_train.py
+++ code/script/bytenet_nltk_comtrans_train.py
@@ -1,12 +1,19 @@
 
 import sugartensor as stf
 
-from code.dataset import NLTKComtrans
+from code.dataset import NLTKComtrans, WMTBilingualNews
 from code.model import ByteNet
+from code.metric import BleuScore, ModelLoss
 
 # set log level to debug
 stf.sg_verbosity(10)
 
-dataset = NLTKComtrans(batch_size=16)
-model = ByteNet(dataset, num_blocks=3, latent_dim=400)
-model.train(lr=0.0001)
+dataset_train = NLTKComtrans(batch_size=16)
+dataset_test = WMTBilingualNews(batch_size=16,
+                                year=2014, source_lang='fr', target_lang='en')
+model = ByteNet(dataset,
+                num_blocks=3, latent_dim=400,
+                save_dir='asset/bytenet_nltk_comtrans')
+model.add_metric(BleuScore(dataset_test))
+model.add_metric(ModelLoss(dataset_test))
+model.train(max_ep=20, lr=0.0001)
