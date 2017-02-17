f0fad0b08af2f5c15278a283015acb4c42cd74aa
Andreas Madsen <amwebdk@gmail.com>
2017-02-17 15:24:16 +0100

    [fix] nltk contrans script

--- code/script/bytenet_nltk_comtrans_train.py
+++ code/script/bytenet_nltk_comtrans_train.py
@@ -11,7 +11,7 @@ stf.sg_verbosity(10)
 dataset_train = NLTKComtrans(batch_size=16)
 dataset_test = WMTBilingualNews(batch_size=16,
                                 year=2014, source_lang='fr', target_lang='en')
-model = ByteNet(dataset,
+model = ByteNet(dataset_train,
                 num_blocks=3, latent_dim=400,
                 save_dir='asset/bytenet_nltk_comtrans')
 model.add_metric(BleuScore(dataset_test))
