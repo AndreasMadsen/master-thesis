18c18a75ce046ba56d686e476e53bce2c83bc6bb
Andreas Madsen <amwebdk@gmail.com>
2017-02-18 14:15:35 +0100

    [report] bytenet hierarchical dilated convolution and full network

---
+++ code/plot/util/ggplot.py
@@ -0,0 +1,55 @@
+
+import sys
+import io
+import os
+import os.path as path
+import tempfile
+import subprocess
+import textwrap
+
+import pandas as pd
+
+thisdir = path.dirname(path.realpath(__file__))
+graphicsdir = path.realpath(
+    path.join(thisdir, '..', '..', '..', 'report', 'graphics')
+)
+
+
+class GGPlot:
+    r_script: str
+
+    def __init__(self, r_script: str):
+        self.r_script = textwrap.dedent(f"""\
+        #!/usr/bin/env Rscript
+        library(ggplot2);
+
+        args = commandArgs(TRUE);
+        dataframe = read.csv(file('stdin'));
+        filepath = args[1];
+
+        {r_script}
+        """)
+
+    def run(self, dataframe: pd.DataFrame, filepath: str):
+        output_file = path.join(graphicsdir, filepath)
+
+        csv_file = io.StringIO()
+        dataframe.to_csv(csv_file)
+
+        with tempfile.NamedTemporaryFile(mode='w') as fd:
+            print(self.r_script, file=fd, flush=True)
+
+            env = os.environ.copy()
+            env['LANGUAGE'] = 'en'
+
+            try:
+                p = subprocess.run(
+                    ['Rscript', fd.name, output_file],
+                    env=env,
+                    input=bytes(csv_file.getvalue(), 'utf8'),
+                    stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                    check=True
+                )
+            except subprocess.CalledProcessError as e:
+                print('Captured R error:', file=sys.stderr)
+                print(str(e.stdout, 'utf-8'), file=sys.stderr)
---
+++ code/plot/wmt-2014-ende-density.py
@@ -0,0 +1,27 @@
+
+import pandas as pd
+import numpy as np
+
+from code.dataset import WMTBilingualNews
+from code.plot.util.ggplot import GGPlot
+
+dataset = WMTBilingualNews(source_lang='de', target_lang='en', year=2014,
+                           min_length=0, max_length=1000)
+
+line_length = []
+
+for i, (source, target) in enumerate(dataset):
+    line_length.append((len(source), 'Deutsch'))
+    line_length.append((len(target), 'English'))
+
+df = pd.DataFrame(line_length, columns=('length', 'language'))
+
+gg = GGPlot("""
+p = ggplot(dataframe, aes(x=length, fill=language))
+p = p + geom_density(alpha=0.5)
+p = p + theme(text = element_text(size=10))
+
+ggsave(filepath, p, width=13, height=6, units="cm");
+""")
+
+gg.run(df, 'theory/wmt-deen-density.pdf')
