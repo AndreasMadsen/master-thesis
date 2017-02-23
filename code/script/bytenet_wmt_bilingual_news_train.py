4c62503dd9e891bc3aca92759f6b1176e7f6dce6
Andreas Madsen <amwebdk@gmail.com>
2017-02-23 20:18:52 +0100

    [fix] update bytenet wmt train script

--- code/script/bytenet_wmt_bilingual_news_train.py
+++ code/script/bytenet_wmt_bilingual_news_train.py
@@ -9,12 +9,23 @@ from code.metric import BleuScore, ModelLoss
 stf.sg_verbosity(10)
 
 dataset_train = WMTBilingualNews(batch_size=16,
-                                 year=2014, source_lang='de', target_lang='en')
-dataset_test = WMTBilingualNews(batch_size=16,
+                                 year=2014,
+                                 source_lang='de', target_lang='en')
+
+dataset_metric = WMTBilingualNews(batch_size=128,
+                                  year=2014,
+                                  source_lang='de', target_lang='en',
+                                  vocabulary=dataset_train.vocabulary,
+                                  validate=True)
+
+dataset_test = WMTBilingualNews(batch_size=128,
+                                year=2015,
+                                source_lang='de', target_lang='en',
                                 vocabulary=dataset_train.vocabulary,
-                                year=2015, source_lang='de', target_lang='en')
+                                validate=True)
 
 model = ByteNet(dataset_train, save_dir='asset/bytenet_wmt_2014')
-model.add_metric(BleuScore(dataset_test))
-model.add_metric(ModelLoss(dataset_test))
+model.add_metric(BleuScore(dataset_metric, name='BLEU-score-train'))
+model.add_metric(BleuScore(dataset_test, name='BLEU-score-test'))
+model.add_metric(ModelLoss(dataset_test, name='model-loss-test'))
 model.train(max_ep=60, lr=0.0001)
