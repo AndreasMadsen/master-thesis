d1b9961ec7238d33316622eb348b756cc9425c03
Andreas Madsen <amwebdk@gmail.com>
2017-03-08 12:51:42 +0100

    [fix] ensure shape inference

--- code/tf_operator/batch_repeat/test/test_batch_repeat.py
+++ code/tf_operator/batch_repeat/test/test_batch_repeat.py
@@ -1,4 +1,6 @@
 
+from nose.tools import assert_equal
+
 import numpy as np
 import tensorflow as tf
 
@@ -10,9 +12,11 @@ def test_output_vector():
     with tf.Session() as sess:
         data = np.asarray([1, 2, 3], dtype=np.float32)
         tensor = tf.placeholder(shape=[None], name='x', dtype=tf.float32)
+        repeat = batch_repeat(tensor, 2)
 
+        assert_equal(repeat.get_shape().as_list(), [None, 2])
         np.testing.assert_almost_equal(
-            sess.run(batch_repeat(tensor, 2), feed_dict={tensor: data}),
+            sess.run(repeat, feed_dict={tensor: data}),
             np.asarray([
                 [data[0], data[0]],
                 [data[1], data[1]],
@@ -30,9 +34,11 @@ def test_output_matrix():
             [7, 8, 9]
         ], dtype=np.float32)
         tensor = tf.placeholder(shape=[None] * 2, name='x', dtype=tf.float32)
+        repeat = batch_repeat(tensor, 2)
 
+        assert_equal(repeat.get_shape().as_list(), [None, 2, None])
         np.testing.assert_almost_equal(
-            sess.run(batch_repeat(tensor, 2), feed_dict={tensor: data}),
+            sess.run(repeat, feed_dict={tensor: data}),
             np.asarray([
                 [data[0], data[0]],
                 [data[1], data[1]],
@@ -55,10 +61,14 @@ def test_output_tensor():
              [34, 35, 36],
              [37, 38, 39]]
         ], dtype=np.float32)
-        tensor = tf.placeholder(shape=[None] * 3, name='x', dtype=tf.float32)
+        tensor = tf.placeholder(
+            shape=[None, None, 3], name='x', dtype=tf.float32
+        )
+        repeat = batch_repeat(tensor, 2)
 
+        assert_equal(repeat.get_shape().as_list(), [None, 2, None, 3])
         np.testing.assert_almost_equal(
-            sess.run(batch_repeat(tensor, 2), feed_dict={tensor: data}),
+            sess.run(repeat, feed_dict={tensor: data}),
             np.asarray([
                 [data[0], data[0]],
                 [data[1], data[1]],
