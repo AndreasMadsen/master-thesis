eebbcfc066b1ae9172571662c64b631be0a9d17c
Andreas Madsen <amwebdk@gmail.com>
2017-03-10 18:29:53 +0100

    [refactor] simplify batch repeat implementation

--- code/tf_operator/batch_repeat/batch_repeat.py
+++ code/tf_operator/batch_repeat/batch_repeat.py
@@ -6,19 +6,11 @@ def batch_repeat(x, repeats=1, name=None):
     with tf.name_scope(name, "batch-repeat", values=[x]):
         x_dims = len(x.get_shape())
 
-        # repeat on the first dimention, (batches * repeats, ...)
-        multiples = [1] * x_dims
-        multiples[0] = repeats
-        t = tf.tile(x, multiples=multiples)
-
-        # reshape into (repeats, batches, ....)
-        shape = tf.concat([[repeats], tf.shape(x)], axis=0)
-        t = tf.reshape(t, shape=shape)
+        # reshape into (batches, 1, ....)
+        t = tf.expand_dims(x, axis=1)
 
         # transpose into (batches, repeats, ....)
-        perm = list(range(x_dims + 1))
-        perm[0] = 1
-        perm[1] = 0
-        t = tf.transpose(t, perm=perm)
+        multiples = [1, repeats] + [1] * (x_dims - 1)
+        t = tf.tile(t, multiples=multiples)
 
         return t
