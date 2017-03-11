640a97e0ade14620bc9c315f70241d0232845bde
Andreas Madsen <amwebdk@gmail.com>
2017-03-11 19:03:09 +0100

    [fix] expose all select_value operators

--- code/tf_operator/select_value/__init__.py
+++ code/tf_operator/select_value/__init__.py
@@ -1,3 +1,5 @@
 # flake8: noqa
 
 from code.tf_operator.select_value.select_dim_value import select_dim_value
+from code.tf_operator.select_value.batch_gather import batch_gather
+from code.tf_operator.select_value.batch_beam_gather import batch_beam_gather
