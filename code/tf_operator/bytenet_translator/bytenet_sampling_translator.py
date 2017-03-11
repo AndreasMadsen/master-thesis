6f871b7d293a51aea94d869aae425816f55ec158
Andreas Madsen <amwebdk@gmail.com>
2017-03-11 19:00:18 +0100

    [feature] update bytenet sampling translator to use beam search

--- code/tf_operator/bytenet_translator/bytenet_sampling_translator.py
+++ code/tf_operator/bytenet_translator/bytenet_sampling_translator.py
@@ -5,17 +5,16 @@ from code.tf_operator.seq_dense import seq_dense
 from code.tf_operator.bytenet_encoder import parallel_bytenet_encoder
 from code.tf_operator.bytenet_decoder \
     import seq_bytenet_decoder, seq_bytenet_decoder_init
-from code.tf_operator.batch_repeat \
-    import batch_repeat, batch_repeat_pack, batch_repeat_unpack
 from code.tf_operator.embedding import embedding_matrix
+from code.tf_operator.beam_search.beam_search_scan import beam_search_scan
 
 
 def bytenet_sampling_translator(x,
                                 latent_dim=20, voca_size=20, num_blocks=3,
                                 rate=[1, 2, 4, 8, 16],
                                 labels=None, container=None,
-                                samples=1, seed=None,
-                                name=None, reuse=None):
+                                beam_size=2, back_prop=True,
+                                name=None, reuse=False):
     with tf.variable_scope(name, "bytenet-sampling-translator",
                            values=[x], reuse=reuse):
         # make embedding matrix for source and target
@@ -37,11 +36,6 @@ def bytenet_sampling_translator(x,
         enc = parallel_bytenet_encoder(enc,
                                        num_blocks=num_blocks, rate=rate,
                                        name="encoder")
-
-        # repeat encoding matrix for paralllel sampling
-        # enc.shape = (batch * repeats, time, dims)
-        enc = batch_repeat(enc, repeats=samples)
-        enc = batch_repeat_pack(enc)
 
         #
         # decode graph ( causal convolution )
@@ -68,17 +62,12 @@ def bytenet_sampling_translator(x,
                 dec, dim=voca_size,
                 name='logits-dense'
             )
-            # sample from softmax distribution
-            # this uses a binary search, the CPU time is:
-            # O(batch * (num_samples * log(num_classes) + num_classes))
-            label_t = tf.squeeze(tf.multinomial(logits_t, 1, seed=seed))
-            label_t = tf.cast(label_t, dtype=tf.int32)
 
-            return (state_t, logits_t, label_t)
+            return (state_t, logits_t)
 
-        (_, logits, labels) = tf.scan(
+        (_, logprops, labels) = beam_search_scan(
             scan_op,
-            elems=tf.transpose(enc, perm=[1, 0, 2]),
+            elems=enc,
             initializer=(
                 init_state,
                 tf.zeros(
@@ -87,12 +76,12 @@ def bytenet_sampling_translator(x,
                 tf.zeros(
                     (tf.shape(enc)[0], ), dtype=tf.int32
                 )  # labels
-            )
+            ),
+            beam_size=beam_size,
+            back_prop=back_prop
         )
 
-        logits = tf.transpose(logits, perm=[1, 0, 2])
-        logits = batch_repeat_unpack(logits, repeats=samples)
-        labels = tf.transpose(labels, perm=[1, 0])
-        labels = batch_repeat_unpack(labels, repeats=samples)
+        # select logprops for the scan final iteration
+        logprops = logprops[:, :, -1]
 
-        return (logits, labels)
+        return (logprops, labels)
