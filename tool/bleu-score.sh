4ead7c55a86589274aa57cf5d969acbeadee10ea
Andreas Madsen <amwebdk@gmail.com>
2017-03-22 13:26:08 +0100

    [feature] restore bleu score bash calculation tools

---
+++ tool/bleu-score.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+while getopts "l:r:t:" opt; do
+  case $opt in
+    l)
+      REF_LANG=$OPTARG
+      ;;
+    r)
+      REF_FILE=$OPTARG
+      ;;
+    t)
+      TRANSLATE_FILE=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+# alias the required perl scripts
+alias multi-bleu='perl code/deps/mosesdecoder/scripts/generic/multi-bleu.perl'
+alias tokenizer='perl code/deps/mosesdecoder/scripts/tokenizer/tokenizer.perl'
+shopt -s expand_aliases
+
+WORK_DIR=`mktemp -d -t blue-score`
+
+# tokenize files
+tokenizer -l $REF_LANG -no-escape < $REF_FILE > $WORK_DIR/tokenized.target.txt 2> /dev/null
+tokenizer -l $REF_LANG -no-escape < $TRANSLATE_FILE > $WORK_DIR/tokenized.translate.txt 2> /dev/null
+
+# compute "multi-bleu" bleu score
+multi-bleu $WORK_DIR/tokenized.target.txt < $WORK_DIR/tokenized.translate.txt
+
+# cleanup workdir
+rm -rf $WORK_DIR
