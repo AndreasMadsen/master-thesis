[fix] disable scale in normalization

code/model/abstract/model.py
@@ -42,7 +42,7 @@ class Model:
         return tf.train.latest_checkpoint(self._save_dir)
 
     def _options_context(self) -> Any:
-        return stf.sg_context(summary=self._deep_summary, scale=False)
+        return stf.sg_context(summary=self._deep_summary)
 
     def add_metric(self, metric: 'code.metric.abstract.Metric') -> None:
         self._metrics.append(metric)
@@ -93,7 +93,7 @@ class Model:
                                      log_device_placement=log_device_placement)
         # XLA will be used on DTU-HPC and will JIT compile GPU kernels
         if 'TF_USE_XLA' in os.environ:
-            config.graph_options.optimizer_options.global_jit_level = \
+            sess_config.graph_options.optimizer_options.global_jit_level = \
                 tf.OptimizerOptions.ON_1
 
         with tf.Session(config=sess_config) as sess: