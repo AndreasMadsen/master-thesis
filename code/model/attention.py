5d3ed32ec9dbcaafea4eb3dc9368202cc6d72aa2
Andreas Madsen <amwebdk@gmail.com>
2017-04-13 13:48:30 +0200

    [cleanup] attention model, step 1

--- code/model/attention.py
+++ code/model/attention.py
@@ -22,108 +22,103 @@ class Attention(Model):
         self.num_blocks = num_blocks
         self.latent_dim = latent_dim
 
-        self._out_help = True
         self._vocab_size = self.dataset.vocabulary_size
         self._batch_size = self.dataset.batch_size
         self._output_sos_id = 0
         self._output_eos_id = 1
 
-    def _build_model(self, batch_size, helper_build_fn, decoder_maxiters=None):
-        # embed input_data into a one-hot representation
-        inputs = tf.one_hot(self.input_data, self._vocab_size)
-        inputs_len = self.input_lengths
+    def _build_model(self, source, length,
+                     helper_build_fn, decoder_maxiters=None):
+        batch_size = source.get_shape().as_list()[0]
+        if batch_size is None:
+            batch_size = tf.shape(source)[0]
 
         with tf.name_scope('encoder'):
+            # embed input_data into a one-hot representation
+            inputs = tf.one_hot(source, self._vocab_size)
+
             fw_cell = rnn.MultiRNNCell([
                 rnn.BasicRNNCell(self.latent_dim) for i in range(self.num_blocks)
             ], state_is_tuple=True)
             bw_cell = rnn.MultiRNNCell([
                 rnn.BasicRNNCell(self.latent_dim) for i in range(self.num_blocks)
             ], state_is_tuple=True)
-            fw_cell_zero = fw_cell.zero_state(batch_size, tf.float32)
-            bw_cell_zero = bw_cell.zero_state(batch_size, tf.float32)
 
             enc_out, _ = tf.nn.bidirectional_dynamic_rnn(
-                fw_cell, bw_cell, inputs,
-                sequence_length=inputs_len,
-                initial_state_fw=fw_cell_zero,
-                initial_state_bw=bw_cell_zero)
+                fw_cell, bw_cell,
+                inputs,
+                sequence_length=length,
+                initial_state_fw=fw_cell.zero_state(batch_size, tf.float32),
+                initial_state_bw=bw_cell.zero_state(batch_size, tf.float32)
+            )
+            enc_out = tf.concat(enc_out, 2)
 
         with tf.name_scope('decoder'):
-            dec_cell_in = rnn.GRUCell(self.latent_dim * 2)
-            attn_values = tf.concat(enc_out, 2)
-            attn_mech = seq2seq.LuongAttention(self.latent_dim * 2, attn_values, inputs_len)
-            dec_cell_attn = rnn.GRUCell(self.latent_dim * 2)
-            dec_cell_attn = seq2seq.DynamicAttentionWrapper(dec_cell_attn, attn_mech, self.latent_dim * 2)
-            dec_cell_out = rnn.GRUCell(self._vocab_size)
-            dec_cell = rnn.MultiRNNCell([dec_cell_in, dec_cell_attn, dec_cell_out],
-                                        state_is_tuple=True)
+            dec_attn = seq2seq.DynamicAttentionWrapper(
+                cell=rnn.GRUCell(self.latent_dim * 2),
+                attention_mechanism=seq2seq.LuongAttention(self.latent_dim * 2, enc_out, length),
+                attention_size=self.latent_dim * 2
+            )
 
-            dec = seq2seq.BasicDecoder(dec_cell, helper_build_fn(),
-                                       dec_cell.zero_state(batch_size, tf.float32))
+            dec_network = rnn.MultiRNNCell([
+                rnn.GRUCell(self.latent_dim * 2),
+                dec_attn,
+                rnn.GRUCell(self._vocab_size)
+            ], state_is_tuple=True)
 
-            dec_out, _ = seq2seq.dynamic_decode(dec, output_time_major=False,
-                    maximum_iterations=decoder_maxiters, impute_finished=True)
+            decoder = seq2seq.BasicDecoder(
+                dec_network, helper_build_fn(),
+                initial_state=dec_network.zero_state(batch_size, tf.float32)
+            )
 
-        self.outputs = dec_out.rnn_output
-        self.output_ids = dec_out.sample_id
+            dec_outputs, _ = seq2seq.dynamic_decode(
+                decoder,
+                maximum_iterations=decoder_maxiters,
+                impute_finished=True
+            )
 
-    def _output_onehot(self, ids):
-        return tf.one_hot(ids, self._vocab_size)
+        return dec_outputs.rnn_output, dec_outputs.sample_id
 
     def loss_model(self,
                    source_all: tf.Tensor, target_all: tf.Tensor,
                    reuse: bool=False) -> Tuple[tf.Tensor, LossesType]:
-        self.input_data = tf.cast(self.dataset.source, tf.int32)
-        self.input_lengths = self.dataset.length
-        self.output_data = tf.cast(self.dataset.target, tf.int32)
-        self.output_lengths = self.dataset.length
-
-        output_data_maxlen = tf.shape(self.output_data)[1]
-
-        def infer_helper():
-            return seq2seq.GreedyEmbeddingHelper(
-                    self._output_onehot,
-                    start_tokens=tf.fill([self._batch_size], self._output_sos_id),
-                    end_token=self._output_eos_id)
+        source = tf.cast(self.dataset.source, tf.int32)
+        target = tf.cast(self.dataset.target, tf.int32)
+        length = self.dataset.length
+        max_length = tf.shape(source)[1]
 
         def train_helper():
-            start_ids = tf.fill([self._batch_size, 1], self._output_sos_id)
-            decoder_input_ids = tf.concat([start_ids, self.output_data], 1)
-            decoder_inputs = self._output_onehot(decoder_input_ids)
-            return seq2seq.TrainingHelper(decoder_inputs, self.output_lengths)
+            batch_size = int(source.get_shape()[0])
+            start_ids = tf.fill([batch_size, 1], self._output_sos_id)
+            decoder_input_ids = tf.concat([start_ids, target], 1)
+            decoder_inputs = tf.one_hot(decoder_input_ids, self._vocab_size)
+            return seq2seq.TrainingHelper(decoder_inputs, length)
 
-        helper = train_helper if self._out_help else infer_helper
+        logits, labels = self._build_model(source, length, train_helper,
+                                           decoder_maxiters=max_length)
+        logits = tf.slice(logits, [0, 0, 0], [-1, max_length, -1])
 
-        self._build_model(self._batch_size, helper, decoder_maxiters=output_data_maxlen)
-
-        output_maxlen = tf.minimum(tf.shape(self.outputs)[1], output_data_maxlen)
-        out_data_slice = tf.slice(self.output_data, [0, 0], [-1, output_maxlen])
-        out_logits_slice = tf.slice(self.outputs, [0, 0, 0], [-1, output_maxlen, -1])
-
-        loss = cross_entropy_direct(out_logits_slice, out_data_slice,
-                                    name='attention')
-        loss = cross_entropy_summary(loss,
-                                     name="supervised-x2y")
+        loss = cross_entropy_direct(logits, target, name='attention')
+        loss = cross_entropy_summary(loss, name="supervised-x2y")
 
         return (loss, [('/cpu:0', loss)])
 
     def greedy_inference_model(self,
                                source: tf.Tensor,
                                reuse: bool=False) -> tf.Tensor:
-        self.input_data = tf.cast(source, tf.int32)
-        self.input_lengths = None
-
-        batch_size = tf.shape(source)[0]
+        source = tf.cast(source, tf.int32)
+        # length # should be passed
+        max_length = tf.shape(source)[1]
 
         def infer_helper():
+            batch_size = tf.shape(source)[0]
             return seq2seq.GreedyEmbeddingHelper(
-                    self._output_onehot,
+                    lambda ids: tf.one_hot(ids, self._vocab_size),
                     start_tokens=tf.fill([batch_size], self._output_sos_id),
                     end_token=self._output_eos_id)
 
-        self._build_model(
-            batch_size, infer_helper, decoder_maxiters=128
+        logits, labels = self._build_model(
+            source, None, infer_helper, decoder_maxiters=max_length
         )
 
-        return self.output_ids
+        return labels
