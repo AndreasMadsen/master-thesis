75490c1a67199ac72e7fcb963e7746a0d1361964
Andreas Madsen <amwebdk@gmail.com>
2017-04-29 21:49:10 +0200

    [fix] update synthetic digits scripts

--- code/script/bytenet_synthetic_digits_test.py
+++ code/script/bytenet_synthetic_digits_test.py
@@ -8,17 +8,18 @@ from code.model import ByteNet
 # set log level to debug
 stf.sg_verbosity(10)
 
-dataset = SyntheticDigits(examples=10, shuffle=False, seed=99)
-model = ByteNet(dataset, num_blocks=3, latent_dim=20,
+dataset_train = SyntheticDigits(batch_size=16, examples=1000, seed=10)
+dataset_test = SyntheticDigits(batch_size=10, examples=128, seed=11,
+                               shuffle=False, repeat=False)
+
+model = ByteNet(dataset_train,
+                num_blocks=3, latent_dim=20,
+                deep_summary=False,
                 save_dir='asset/bytenet_synthetic_digits')
 
-export = ExportDataset(dataset)
-export.train()
+translation_tuple = model.predict_from_dataset(dataset_test, samples=10)
 
-test_predict = model.predict(export.sources)
-
-for i, (source, target, predict) in \
-        enumerate(zip(export.sources, export.targets, test_predict)):
+for i, (source, target, translation) in zip(range(10), translation_tuple):
     print('  %d  source: %s' % (i, source))
     print('     target: %s' % (target, ))
-    print('    predict: %s' % (predict, ))
+    print('    predict: %s' % (translation, ))
--- code/script/bytenet_synthetic_digits_train.py
+++ code/script/bytenet_synthetic_digits_train.py
@@ -8,12 +8,16 @@ from code.metric import MisclassificationRate, ModelLoss
 # set log level to debug
 stf.sg_verbosity(10)
 
-dataset = SyntheticDigits(batch_size=16, examples=1000, seed=10)
-dataset_test = SyntheticDigits(batch_size=50, examples=50, seed=11)
+dataset_train = SyntheticDigits(batch_size=16, examples=128, seed=10)
+dataset_test = SyntheticDigits(batch_size=16, examples=128, seed=11)
 
-model = ByteNet(dataset, num_blocks=3, latent_dim=20,
+model = ByteNet(dataset_train,
+                num_blocks=3, latent_dim=20,
                 deep_summary=False,
                 save_dir='asset/bytenet_synthetic_digits')
+
+model.add_metric(MisclassificationRate(dataset_train))
 model.add_metric(MisclassificationRate(dataset_test))
 model.add_metric(ModelLoss(dataset_test))
-model.train(max_ep=2000, lr=0.01, log_interval=10, save_interval=30)
+
+model.train(max_ep=300, lr=0.01, log_interval=10, save_interval=30)
