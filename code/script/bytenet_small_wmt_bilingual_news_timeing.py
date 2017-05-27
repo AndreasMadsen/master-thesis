7ffb67f018189864e0803e4e4b6aff327697c5fe
Andreas Madsen <amwebdk@gmail.com>
2017-05-27 18:30:40 +0200

    [jobs] add bytenet-small jobs

---
+++ code/script/bytenet_small_wmt_bilingual_news_timeing.py
@@ -0,0 +1,28 @@
+
+import sugartensor as stf
+
+from code.dataset import WMTBilingualNews
+from code.model import ByteNet
+from code.metric import BleuScore, ModelLoss
+
+# set log level to debug
+stf.sg_verbosity(10)
+
+dataset_train = WMTBilingualNews(batch_size=64,
+                                 year=2014,
+                                 source_lang='de', target_lang='en',
+                                 min_length=None, max_length=None)
+
+dataset_test = WMTBilingualNews(batch_size=128,
+                                year=2015,
+                                source_lang='de', target_lang='en',
+                                min_length=None, max_length=None,
+                                vocabulary=dataset_train.vocabulary,
+                                validate=True)
+
+model = ByteNet(dataset_train, save_dir='asset/bytenet_small_wmt_2014_timeing/bytenet_wmt_2014_gpu4_timeing',
+                deep_summary=False, gpus=4)
+model.add_metric(BleuScore(dataset_train, name='BLEU-score-train'))
+model.add_metric(BleuScore(dataset_test, name='BLEU-score-test'))
+model.add_metric(ModelLoss(dataset_test, name='model-loss-test'))
+model.train(max_ep=300, lr=0.0001)
