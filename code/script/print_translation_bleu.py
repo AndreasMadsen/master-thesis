6a4f67d0eb33ef315a660eca1e0e489f0d1cb71c
Andreas Madsen <amwebdk@gmail.com>
2017-06-02 18:05:12 +0200

    [feature] add translation scripts

---
+++ code/script/print_translation_bleu.py
@@ -0,0 +1,24 @@
+
+import itertools
+import json
+import re
+
+import tqdm
+
+from code.moses import Tokenizer, multi_bleu
+
+# tokenize data
+target_tokenizer = Tokenizer('en')
+translation_tokenizer = Tokenizer('en')
+
+with open('result/translation/wmt-dump.json') as translations:
+    with target_tokenizer, translation_tokenizer:
+
+        for translation_raw in tqdm.tqdm(list(translations)):
+            translation_tuple = json.loads(translation_raw)
+
+            target_tokenizer.write(translation_tuple['target'])
+            translation_tokenizer.write(translation_tuple['translation'])
+
+# calculate BLEU score
+print(multi_bleu(translate=translation_tokenizer, target=target_tokenizer))
